3450a9fc7b02fed1a0d043fd91bec5d0834b1556
ktatu <kaikkonentatu@gmail.com>
2023-01-16 02:46:15 +0200

    Deployed to fly.io

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require("express")
-const app = express()
+import express from "express"
+import cors from "cors"
+
+export const app = express()
+app.use(cors())
 
 app.use(express.static("build"))
 
-const PORT = 3001
+app.get("/api", (req, res) => {
+    res.send("<h1>test</h1>")
+})
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log("server running on port ", PORT)
 })
