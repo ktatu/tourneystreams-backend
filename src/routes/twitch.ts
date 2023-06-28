5e1cf7bf1d6c06ede95ef1aeafa4dfcebb08656d
ktatu <kaikkonentatu@gmail.com>
2023-06-28 21:22:55 +0300

    Query params remain throughout the oauth2 process

--- src/routes/twitch.ts
+++ src/routes/twitch.ts
@@ -1,24 +1,42 @@
 import express from "express"
 import passport from "passport"
 import { CLIENT_URL } from "../envConfig"
+import { format } from "url"
 
 const router = express.Router()
 
-router.get("/auth", passport.authenticate("twitch", { scope: "user:read:follows", session: false }))
+// haetaan seuratut kanavat
+router.get("/", (req, res) => {
+    console.log("req query ", req.query)
+    res.status(200).send("ok")
+})
+
+//router.get("/auth", passport.authenticate("twitch", { scope: "user:read:follows", session: false }))
+
+router.get("/auth", (req, res) => {
+    const authenticator = passport.authenticate("twitch", {
+        scope: "user:read:follows",
+        session: false,
+        state: JSON.stringify(req.query),
+    })
+
+    authenticator(req, res)
+})
 
 router.get(
     "/redirect",
-    passport.authenticate("twitch", { failureRedirect: "/", session: false }),
+    passport.authenticate("twitch", { failureRedirect: "http://localhost:3000", session: false }),
     (req, res) => {
         if (!req.user) {
             return res.status(500)
         }
 
-        //console.log("twitch token ", req.user.twitchToken)
-        console.log("req query ", req.query)
+        const { state } = req.query
+        const urlString = format({ pathname: CLIENT_URL, query: JSON.parse(state as string) })
 
         res.cookie("twitch-token", req.user.twitchToken)
-        return res.redirect(CLIENT_URL)
+
+        return res.redirect(urlString)
     }
 )
 
