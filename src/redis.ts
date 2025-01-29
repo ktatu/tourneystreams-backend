d5ea37904ef3fbb490aa02544ac5ad469a4c0fd0
ktatu <kaikkonentatu@gmail.com>
2025-01-29 14:51:25 +0200

    refactor

--- src/redis.ts
+++ src/redis.ts
@@ -3,9 +3,4 @@ import { REDIS_URL } from "./envConfig.js"
 
 const redis = createClient({ url: REDIS_URL })
 
-redis.on("error", (err) => console.log("Redis Client Error", err))
-redis.on("connect", () => console.log("Connected to redis"))
-
-await redis.connect()
-
 export default redis
