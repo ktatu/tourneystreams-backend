451b166774ad50039c8955da0a77fee806baf9e1
ktatu <kaikkonentatu@gmail.com>
2026-09-25 20:00:55 +0300

    Update env + redis config

--- src/redis.ts
+++ src/redis.ts
@@ -1,13 +1,15 @@
 import { createClient } from "redis"
-import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, REDIS_USERNAME } from "./envConfig.js"
+import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, REDIS_URL, REDIS_USERNAME } from "./envConfig.js"
 
-const redis = createClient({
-    username: REDIS_USERNAME,
-    password: REDIS_PASSWORD,
-    socket: {
-        host: REDIS_HOST,
-        port: REDIS_PORT,
-    },
-})
+const redis = REDIS_URL
+    ? createClient({ url: REDIS_URL })
+    : createClient({
+          username: REDIS_USERNAME,
+          password: REDIS_PASSWORD,
+          socket: {
+              host: REDIS_HOST,
+              port: REDIS_PORT,
+          },
+      })
 
 export default redis
